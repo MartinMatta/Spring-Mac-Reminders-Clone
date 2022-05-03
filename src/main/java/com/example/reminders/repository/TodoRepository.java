9db8416ae246dc1491a669b3d237a2af06098c8f
Martin Matta <martin.matta@unicorn.com>
2022-05-03 12:00:50 +0200

    add @Repository in TodoRepository.java

--- src/main/java/com/example/reminders/repository/TodoRepository.java
+++ src/main/java/com/example/reminders/repository/TodoRepository.java
@@ -2,5 +2,7 @@ package com.example.reminders.repository;
 
 import com.example.reminders.domain.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {}
