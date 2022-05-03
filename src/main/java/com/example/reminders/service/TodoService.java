2090535257197a649626ded425fbaef0e85244e0
Martin Matta <martin.matta@unicorn.com>
2022-05-03 11:59:42 +0200

    add Todo service

---
+++ src/main/java/com/example/reminders/service/TodoService.java
@@ -0,0 +1,11 @@
+package com.example.reminders.service;
+
+import com.example.reminders.domain.Todo;
+
+import java.util.List;
+
+public interface TodoService {
+
+    List<Todo> getAllTodos();
+
+}
---
+++ src/main/java/com/example/reminders/service/TodoServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.reminders.service;
+
+import com.example.reminders.domain.Todo;
+import com.example.reminders.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoServiceImpl implements TodoService {
+
+    private TodoRepository todoRepository;
+
+    @Autowired
+    public TodoServiceImpl(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    @Override
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+}
